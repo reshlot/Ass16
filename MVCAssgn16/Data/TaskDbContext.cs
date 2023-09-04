a255adeb6018449a06e2c789bc80db6f23bd25f7
HP <reshmaksriet@gmail.com>
2023-09-04 15:52:26 +0530

    Initial commit

---
+++ MVCAssgn16/Data/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCAssgn16.Models;
+
+namespace MVCAssgn16.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext (DbContextOptions<TaskDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCAssgn16.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ MVCAssgn16/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCAssgn16.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
